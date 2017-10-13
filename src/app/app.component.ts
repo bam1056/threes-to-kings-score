903d35316087fb5d1755ba8ce426a2fb1d3c067d
Brett Macy <bam03584@aol.com>
2017-10-13 18:58:42 -0400

    final draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ApplicationRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { cards } from '../models/cards.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  gameStarted = false;
+  cards = cards;
+  players = [];
+  playerForm: NgForm;
+  ps = {
+    player1: null,
+    player2: null,
+    player3: null,
+    player4: null,
+    player5: null,
+    player6: null,
+    player7: null,
+    player8: null
+  };
+
+  constructor(private _appRef: ApplicationRef) { }
+
+  onSubmit(form: NgForm) {
+    this.playerForm = form;  // _.cloneDeep(form);
+    Object.keys(this.ps).forEach(p => {
+      if (this.ps[p]) {
+        this.players.push(this.ps[p]);
+      }
+    });
+    this.gameStarted = true;
+  }
+
+  reset() {
+    this.gameStarted = false;
+    this.playerForm.reset();
+    this.players = [];
+    Object.keys(this.ps).forEach(p => {
+      delete this.ps[p];
+    });
+  }
 }
