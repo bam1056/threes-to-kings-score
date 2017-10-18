40cf63d3f3756ac5cc33eed2b34483ab1f43f29b
Brett Macy <brettm@itworks.com>
2017-10-18 17:39:09 -0400

    added in total, still not working correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ApplicationRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { cards } from '../models/cards.model';
+import { Cards } from './models/card.model';
 import * as _ from 'lodash';
 
 @Component({
@@ -10,30 +10,36 @@ import * as _ from 'lodash';
 })
 export class AppComponent {
   gameStarted = false;
-  cards = cards;
+  cards = Cards;
   players = [];
   playerForm: NgForm;
   ps = {
-    player1: null,
-    player2: null,
-    player3: null,
-    player4: null,
-    player5: null,
-    player6: null,
-    player7: null,
-    player8: null
+    player1: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player2: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player3: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player4: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player5: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player6: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player7: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null },
+    player8: { name: null, score: Array.apply(null, Array(11)).map(function () {}), totalScore: null }
   };
 
   constructor(private _appRef: ApplicationRef) { }
 
   onSubmit(form: NgForm) {
-    this.playerForm = form;  // _.cloneDeep(form);
+    this.playerForm = form;
     Object.keys(this.ps).forEach(p => {
-      if (this.ps[p]) {
+      if (this.ps[p].name) {
         this.players.push(this.ps[p]);
       }
     });
     this.gameStarted = true;
+    console.log(this.players);
+  }
+
+  calculateTotal(player: object) {
+    console.log(player);
+    player['totalScore'] = player['score'].reduce((c, p) => c + p);
   }
 
   reset() {
---
+++ src/app/models/card.model.ts
@@ -0,0 +1,13 @@
+export const Cards = [
+    { name: 'Three', value: 3 },
+    { name: 'Four', value: 4 },
+    { name: 'Five', value: 5 },
+    { name: 'Six', value: 6 },
+    { name: 'Seven', value: 7 },
+    { name: 'Eight', value: 8 },
+    { name: 'Nine', value: 9 },
+    { name: 'Ten', value: 10 },
+    { name: 'Jack', value: 'Jack' },
+    { name: 'Queen', value: 'Queen' },
+    { name: 'King', value: 'King' }
+];
